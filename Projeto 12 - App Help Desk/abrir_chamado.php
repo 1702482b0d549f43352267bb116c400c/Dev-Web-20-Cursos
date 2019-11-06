db8ce45324cc0f0b821345afa6141defd7cf88aa
taniacristina <roc.ta.cris@gmail.com>
2019-11-06 11:28:07 -0300

    Adicionando validador de acesso e rotas entre páginas

--- Projeto 12 - App Help Desk/abrir_chamado.php
+++ Projeto 12 - App Help Desk/abrir_chamado.php
@@ -1,10 +1,4 @@
-<?php
-  session_start();
-  if(!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] != 'SIM') {
-    header('Location: index.php?login=erro2');
-  }
-  
-?>
+<?php require_once "validador_acesso.php" ?>
 
 <html>
   <head>
@@ -67,7 +61,7 @@
 
                     <div class="row mt-5">
                       <div class="col-6">
-                        <button class="btn btn-lg btn-warning btn-block" type="submit">Voltar</button>
+                        <a href="home.php" class="btn btn-lg btn-warning btn-block">Voltar</a>
                       </div>
 
                       <div class="col-6">
