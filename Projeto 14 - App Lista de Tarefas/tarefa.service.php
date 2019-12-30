8946d0f720f34eef06ee2d03481d2822fa692bdc
MatheusEdnei <ednei880@gmail.com>
2019-12-30 16:49:40 -0300

    Finalizando projeto 14

--- Projeto 14 - App Lista de Tarefas/tarefa.service.php
+++ Projeto 14 - App Lista de Tarefas/tarefa.service.php
@@ -30,8 +30,22 @@
 			return $stmt->fetchAll(PDO::FETCH_OBJ);
 		}
 
-		public function atualizar(){}
-		public function remover(){}
+		public function atualizar(){
+			$query = 'UPDATE tb_tarefas SET tarefa = :tarefa
+					WHERE id= :id';
+			$stmt = $this->conexao->prepare($query);
+			$stmt->bindValue(':tarefa', $this->tarefa->__get('tarefa'));
+			$stmt->bindValue(':id', $this->tarefa->__get('id'));
+			return $stmt->execute();
+
+		}
+		
+		public function remover(){
+			$query = 'DELETE FROM tb_tarefas WHERE id = :id';
+			$stmt = $this->conexao->prepare($query);
+			$stmt->bindValue(':id', $this->tarefa->__get('id'));
+			$stmt->execute();
+		}
 
 	}
 	
